77569bd1b922f386ca87e46fbba9157c0dbeea45
nader <nader@DESKTOP-4HJADNI>
2019-08-11 17:10:50 +0430

    first

---
+++ Jwt/Controllers/TokenController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Jwt.Controllers
+{
+    public class TokenController : ControllerBase
+    {
+        private const string SECRET_KEY = "TQvgjeABMPOwCycOqah5EQu5yyVjpmVG";
+        public static readonly SymmetricSecurityKey SigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(SECRET_KEY));
+        [HttpGet]
+        [Route("api/token/{u}/{p}")]
+        public IActionResult Get(string u, string p)
+        {
+            return new ObjectResult(GenerateToken(u));
+        }
+
+        private string GenerateToken(string u)
+        {
+            var token = new JwtSecurityToken(
+                claims: new Claim[] {
+                    new Claim(ClaimTypes.Name,u)
+                },
+                notBefore: new DateTimeOffset(DateTime.Now).DateTime,
+                expires: new DateTimeOffset(DateTime.Now.AddMinutes(60)).DateTime,
+                signingCredentials: new SigningCredentials(SigningKey, SecurityAlgorithms.HmacSha256)
+                );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+
+        }
+    }
+}
